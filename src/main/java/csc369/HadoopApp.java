7fef8be2820d160dab5b2c2c081595c5b7e35b93
andandandhi <andrewducdanguniv@gmail.com>
2023-07-10 16:13:05 -0700

    starter file

--- src/main/java/csc369/HadoopApp.java
+++ src/main/java/csc369/HadoopApp.java
@@ -33,7 +33,7 @@ public class HadoopApp {
 	    job.setMapperClass(AccessLog.MapperImpl.class);
 	    job.setOutputKeyClass(AccessLog.OUTPUT_KEY_CLASS);
 	    job.setOutputValueClass(AccessLog.OUTPUT_VALUE_CLASS);
-        } else if ("AccessLog2".equalsIgnoreCase(otherArgs[0])) {
+	} else if ("AccessLog2".equalsIgnoreCase(otherArgs[0])) {
 	    job.setReducerClass(AccessLog2.ReducerImpl.class);
 	    job.setMapperClass(AccessLog2.MapperImpl.class);
 	    job.setOutputKeyClass(AccessLog2.OUTPUT_KEY_CLASS);
